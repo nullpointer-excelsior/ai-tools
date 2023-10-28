b7149cfae6f5cfe50a8778ede3d856dd82958e62
nullpointer-excelsior <red.tiger.developer@proton.me>
2023-10-27 22:02:27 -0300

    text templates refactorized

--- src/blogger.py
+++ src/blogger.py
@@ -1,59 +1,17 @@
+import sys, select, re
 from datetime import datetime
-import sys, select
 from pwn import log
-import re
-
 from libs.openai_api import ChatGPTModel, get_completion
 from libs.utils import truncate_text
 
-
-def read_argument():
-    return sys.argv[1] if len(sys.argv) > 1 else None
-
-
-def read_stdin():
-    if select.select([sys.stdin,],[],[],0.0)[0]:
-        return sys.stdin.read().strip()
-    return None
-
-
-def read_file(filename):
-    with open(filename, "r") as file:
-        return file.read()
-
-
-def read_content():
-    filename_input = read_argument()
-    if filename_input is None:
-        return read_stdin()
-    return read_file(filename_input)
-
-
-def correct_grammar_by_ai(content):
-    prompt = f"""
+CORRECT_TEXT_PROMPT = """
 Corrige gramaticalmente el siguiente artículo encerrado en triple acento grave, el cual está en formato markdown. Ignora los bloques de código, pero deben estar incluidos en el artículo final.
 ```{content}```
-    """
-    return get_completion(prompt=prompt,model=ChatGPTModel.GPT_3_5_TURBO_16K)
-    
+"""
 
-def find_first_title(text):
-    pattern = r'^\s*#.*'  # Expresión regular que busca líneas que comiencen con '#'
-    match = re.search(pattern, text, re.MULTILINE)
-    if match:
-        return match.group().replace('#', '').strip()
-    return None
+CATEGORIES_PROMPT = """
+De la siguiente lista de titulos encerradas en triple acento grave, elige de la siguiente lista:
 
-
-def find_all_titles(markdown_text):
-    header_pattern = r'^(#+)\s+(.*?)\n'  # Patrón para encontrar títulos (##, ###, etc.)
-    headers = re.findall(header_pattern, markdown_text, re.MULTILINE)
-    headers = [header[1] for header in headers]
-    return headers
-
-
-def create_categories_by_ai(titles):
-    predefined_cetegories = """
 AI
 Angular
 Arquitectura
@@ -88,12 +46,68 @@ ORM
 Software
 Software Engineer
 
+las categorias mas adecuadas para un articulo de blog de ingeniera de software separados por coma y capitalizadas. 
+response de forma directa, sin explicaciones y en el siguiente formato: ```category1,categor2,...categoryN```.
+```{titles}```
 """
-    prompt = f"""
-    De la siguiente lista de titulos encerradas en triple acento grave, elige de la siguiente lista ```{predefined_cetegories}``` las categorias mas adecuadas 
-    para un articulo de blog de ingeniera de software separados por coma y capitalizadas. response de forma directa, sin explicaciones y en el siguiente formato: ```category1,categor2,...categoryN```.
-    ```{titles}```
-    """
+
+MARKDOWN_PAGE_TEMPLATE = """---
+title: {title}
+author: Benjamin
+date: {date} -0500
+categories: {categories}
+tags: {tags}
+---
+
+{content}
+
+"""
+
+
+def read_argument():
+    return sys.argv[1] if len(sys.argv) > 1 else None
+
+
+def read_stdin():
+    if select.select([sys.stdin,],[],[],0.0)[0]:
+        return sys.stdin.read().strip()
+    return None
+
+
+def read_file(filename):
+    with open(filename, "r") as file:
+        return file.read()
+
+
+def read_content():
+    filename_input = read_argument()
+    if filename_input is None:
+        return read_stdin()
+    return read_file(filename_input)
+
+
+def correct_grammar_by_ai(content):
+    prompt = CORRECT_TEXT_PROMPT.format(content=content)
+    return get_completion(prompt=prompt,model=ChatGPTModel.GPT_3_5_TURBO_16K)
+    
+
+def find_first_title(text):
+    pattern = r'^\s*#.*'  # Expresión regular que busca líneas que comiencen con '#'
+    match = re.search(pattern, text, re.MULTILINE)
+    if match:
+        return match.group().replace('#', '').strip()
+    return None
+
+
+def find_all_titles(markdown_text):
+    header_pattern = r'^(#+)\s+(.*?)\n'  # Patrón para encontrar títulos (##, ###, etc.)
+    headers = re.findall(header_pattern, markdown_text, re.MULTILINE)
+    headers = [header[1] for header in headers]
+    return headers
+
+
+def create_categories_by_ai(titles):
+    prompt = CATEGORIES_PROMPT.format(titles=titles)
     return get_completion(prompt=prompt)
 
 
@@ -112,17 +126,14 @@ def create_post(title, categories, content: str):
     categories_text = str(categories).replace("'", "")
     tags = categories_text.lower()
     title_text = title.replace(':', '')
-    template=f"""---
-title: {title_text}
-author: Benjamin
-date: {datetime.now().strftime('%Y-%m-%d 00:00:00')} -0500
-categories: {categories_text}
-tags: {tags}
----
-
-{content_text}
-
-    """
+    date = datetime.now().strftime('%Y-%m-%d 00:00:00')
+    template = MARKDOWN_PAGE_TEMPLATE.format(
+        content=content_text, 
+        categories=categories_text, 
+        tags=tags, 
+        title=title_text,
+        date=date
+    )
     filename = generate_post_file(title)
     with open(filename, "w") as file:
         file.write(template)
